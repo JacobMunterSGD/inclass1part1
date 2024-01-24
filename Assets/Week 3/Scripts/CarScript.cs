3a473b21d4ce20a8387c5d68ed7b176c928f6ac3
munter <munter@sheridancollege.ca>
2024-01-24 11:09:26 -0500

    Task 3 complete: car controller

---
+++ Assets/Week 3/Scripts/CarScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+
+    public float steeringSpeed;
+    public float forwardSpeed;
+    public float maxSpeed;
+
+    public Rigidbody2D rigidBody;
+
+    float steering;
+    float acceleration;
+
+
+    void Update()
+    {
+
+        steering = Input.GetAxis("Horizontal");
+        acceleration = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.AddTorque(steering * steeringSpeed * Time.deltaTime);
+
+        Vector2 force = transform.up * forwardSpeed * acceleration * Time.deltaTime;
+
+        if (rigidBody.velocity.magnitude < maxSpeed)
+        {
+
+            rigidBody.AddForce(force);
+
+        }
+
+    }
+}
